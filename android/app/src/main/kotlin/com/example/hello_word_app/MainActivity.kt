ba6f242995b0086997448045acdd80c44adf3e46
Diannyz Ortega <diannyz61@gmail.com>
2023-10-20 19:48:48 -0400

    Subida de la app de flutter al repositorio

---
+++ android/app/src/main/kotlin/com/example/hello_word_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hello_word_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:hello_word_app/presentation/screens/counter/counter_functions_screen.dart';
+//import 'package:hello_word_app/presentation/screens/counter/counter_screen.dart';
+
+void main(){
+  runApp( const MyApp() );
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      useMaterial3: true,
+      colorSchemeSeed: Colors.blueGrey
+    ),
+    home: const CounterFuctionsScreen()
+   );
+  }
+}
