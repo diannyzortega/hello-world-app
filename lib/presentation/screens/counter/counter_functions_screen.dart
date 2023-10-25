a7d4e6de44b8fbe76555396f349ddecb4dae785e
EdisonDavidCode <edisondavidr.rd@gmail.com>
2023-10-24 22:10:34 -0400

    Widgets personalizados cap. 54

--- lib/presentation/screens/counter/counter_functions_screen.dart
+++ lib/presentation/screens/counter/counter_functions_screen.dart
@@ -46,36 +46,30 @@ class _CounterFuctionsScreenState extends State<CounterFuctionsScreen> {
         floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            FloatingActionButton(
-              shape: const StadiumBorder(),
-              onPressed: () {
-                setState(() {
-                  clickCounter = 0;
-                });
-              },
-              child: const Icon(Icons.refresh_outlined),
-            ),
+            CustomButton(icon: Icons.refresh_rounded),
             const SizedBox(height: 10),
-            FloatingActionButton(
-              shape: const StadiumBorder(),
-              onPressed: () {
-                setState(() {
-                  clickCounter += 1;
-                });
-              },
-              child: const Icon(Icons.plus_one),
-            ),
+            CustomButton(icon: Icons.exposure_minus_1_outlined),
             const SizedBox(height: 10),
-            FloatingActionButton(
-              shape: const StadiumBorder(),
-              onPressed: () {
-                setState(() {
-                  clickCounter--;
-                });
-              },
-              child: const Icon(Icons.exposure_minus_1_outlined),
-            ),
+            CustomButton(icon: Icons.plus_one),
           ],
         ));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+
+  const CustomButton({
+    super.key,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      shape: const StadiumBorder(),
+      onPressed: () {},
+      child: Icon(icon),
+    );
   }
 }
