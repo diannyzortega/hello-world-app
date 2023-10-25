28f3be3c0615f798095b2a468644abc8bd8d6272
Diannyz Ortega <diannyz61@gmail.com>
2023-10-24 20:33:02 -0400

    Widgets Personalizados - cap 54

--- lib/presentation/screens/counter/counter_functions_screen.dart
+++ lib/presentation/screens/counter/counter_functions_screen.dart
@@ -36,34 +36,33 @@ class _CounterFuctionsScreenState extends State<CounterFuctionsScreen> {
               'Click${clickCounter == 1 ? '' : 's'}',
               style: const TextStyle(fontSize: 25),
             )
-
-            //    if ( clickCounter == 1)
-            //   const Text('Click', style: TextStyle( fontSize: 25),)
-            //     else
-            // const Text('Clicks', style: TextStyle( fontSize: 25),)
           ],
         )),
         floatingActionButton: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
-            FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  clickCounter += 1;
-                });
-              },
-              child: const Icon(Icons.plus_one),
-            ),
+            CustomButton(icon: Icons.refresh_outlined),
             const SizedBox(height: 10),
-            FloatingActionButton(
-              onPressed: () {
-                setState(() {
-                  clickCounter--;
-                });
-              },
-              child: const Icon(Icons.exposure_minus_1_outlined),
-            ),
+            CustomButton(icon: Icons.exposure_minus_1_outlined),
+            const SizedBox(height: 10),
+            CustomButton(icon: Icons.plus_one),
           ],
         ));
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  const CustomButton({
+    super.key,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: () {},
+      child: Icon(icon),
+    );
   }
 }
